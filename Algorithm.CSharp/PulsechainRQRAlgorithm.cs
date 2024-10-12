939360f69d218bf15c34c498de20e46269c46af6
Istvan Agoston <lee.nover@gmail.com>
2024-10-12 10:05:44 +0200

    added option to consolidate data

--- Algorithm.CSharp/PulsechainRQRAlgorithm.cs
+++ Algorithm.CSharp/PulsechainRQRAlgorithm.cs
@@ -65,6 +65,7 @@ namespace QuantConnect.Algorithm.CSharp
         private string PairMainUSD;
         decimal MAIN_RESERVE = 100000m;
         decimal OTHER_RESERVE = 0m;
+        private bool consolidate = true;
         public override void Initialize()
         {
             SetStartDate(2024, 02, 02);
@@ -81,9 +82,12 @@ namespace QuantConnect.Algorithm.CSharp
             PairOtherMain = OTHER + MAIN;
             PairMainUSD = MAIN + "USD";
 
-            cryptoOtherMain = AddCrypto(PairOtherMain, Resolution.Hour, MARKET);
-            cryptoMainUSD = AddCrypto(PairMainUSD, Resolution.Hour, MARKET);
+            UniverseSettings.Resolution = Resolution.Minute;
+            cryptoOtherMain = AddCrypto(PairOtherMain, consolidate ? Resolution.Minute : Resolution.Hour, MARKET);
+            cryptoMainUSD = AddCrypto(PairMainUSD, consolidate ? Resolution.Minute : Resolution.Hour, MARKET);
             symbolOtherMain = cryptoOtherMain.Symbol;
+            symbolMainUSD = cryptoMainUSD.Symbol;
+            SetBenchmark(symbolOtherMain);
             SetCash(MAIN, GetParameter("cash-main", 2000000m));
             SetCash(OTHER, GetParameter("cash-other", 100000m));
             var rqrPeriod = GetParameter("rqr-period", 10);
@@ -94,21 +98,29 @@ namespace QuantConnect.Algorithm.CSharp
             rqrMainUSD = new RationalQuadraticRegression(rqrPeriod, rqrLookback, rqrWeight);
             RegisterIndicator(symbolOtherMain, rqrOtherMain, null);
             RegisterIndicator(cryptoMainUSD.Symbol, rqrMainUSD, null);
-            SetWarmUp(10);
+            SetWarmUp(rqrPeriod + rqrLookback);
             
             /*var chart = new Chart("Price");
             AddChart(chart);
             chart.AddSeries(new Series(rqr.Name, SeriesType.Line, "$", Color.Orange));
             chart.AddSeries(new CandlestickSeries(OTHER, OTHER));*/
-
-            //Consolidate(symbol, TimeSpan.FromMinutes(5), OnDataConsolidated);
+            if (consolidate)
+            {
+                Consolidate(cryptoMainUSD.Symbol, TimeSpan.FromMinutes(5), OnDataConsolidated);
+                Consolidate(cryptoOtherMain.Symbol, TimeSpan.FromMinutes(5), OnDataConsolidated);
+            }
         }
 
         private void OnDataConsolidated(TradeBar bar)
         {
-            return;
+            if (bar.Symbol == cryptoMainUSD.Symbol)
+            {
+                rqrMainUSD.Update(Time, bar.Close);
+                return;
+            }
+            
             rqrOtherMain.Update(Time, bar.Close);
-            if (!rqrOtherMain.IsReady) return;
+            if (IsWarmingUp || !rqrOtherMain.IsReady) return;
             
             var price = Securities[PairOtherMain].Price;
             var isBullish = rqrOtherMain[0].Value > rqrOtherMain[1].Value;
@@ -138,7 +150,8 @@ namespace QuantConnect.Algorithm.CSharp
         /// Slice object keyed by symbol containing the stock data
         public override void OnData(Slice data)
         {
-            if (IsWarmingUp) return;
+            if (IsWarmingUp || consolidate) return;
+
             var price = cryptoOtherMain.Price;// Securities[PAIR].Price;
             var isBullish = rqrOtherMain[0].Value > rqrOtherMain[1].Value;
             var isBearish = rqrOtherMain[0].Value < rqrOtherMain[1].Value;
@@ -181,5 +194,7 @@ namespace QuantConnect.Algorithm.CSharp
             Log($"{Time} - TotalPortfolioValue: {Portfolio.TotalPortfolioValue}");
             Log($"{Time} - CashBook: \r\n{Portfolio.CashBook}");
         }
+
+        
     }
 }
